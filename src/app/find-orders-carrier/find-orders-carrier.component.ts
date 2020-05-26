913672e349b2d6df6a0ab02ef1ad7286a5655abc
juanlato <juandavidxd998@gmail.com>
2020-05-26 12:08:40 -0500

    Googlemaps

--- src/app/find-orders-carrier/find-orders-carrier.component.ts
+++ src/app/find-orders-carrier/find-orders-carrier.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { GoogleMap, MapInfoWindow, MapMarker } from "@angular/google-maps";
 @Component({
   selector: 'app-find-orders-carrier',
   templateUrl: './find-orders-carrier.component.html',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindOrdersCarrierComponent implements OnInit {
 
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
+
+  zoom = 18;
+  center: google.maps.LatLngLiteral;
+  options: google.maps.MapOptions = {
+    zoomControl: true,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    mapTypeId: "hybrid"
+  };
+  markers = [];
+  infoContent = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition(x => {
+      this.center = {
+        lat: x.coords.latitude,
+        lng: x.coords.longitude
+      };
+      this.markers.push({
+        position: {
+          lat: x.coords.latitude,
+          lng: x.coords.longitude
+        },
+        label: {
+          color: "blue",
+          text: "Home"
+        },
+        title: "Marker Title",
+        info: "Home marquer ",
+        options: {
+          animation: google.maps.Animation.BOUNCE
+        }
+      });
+    });
+  }
+  openInfo(marker: MapMarker, info) {
+    this.infoContent = info;
+     this.info.open(marker);
   }
 
 }
